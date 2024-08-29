a39b7071147d3490d70988d4293115fd2d37232c
idatufilli <idatufilli@live.it>
2024-08-29 12:42:27 +0200

    fine

--- popup/training.js
+++ popup/training.js
@@ -1,63 +1,63 @@
 // Promot per il Few_Shot
 const Few_Shot = [
-    `Exemple 1: Text of privacy policy: Our service collects user information to improve the overall experience. We share this information with selected partners to provide additional services. Users can manage their preferences through their account settings. We retain information until the user decides to delete their account. We protect information through encryption. We will notify users in case of changes to the policy. We do not respond to Do Not Track signals. Data collection practices for children comply with local regulations.
+    `Exemple 1: Text of privacy policy: Welcome to Instagram (\"Instagram,\" \"we,\" \"us\" or \"our\"). Instagram provides a fast, beautiful and fun way for you to share media through our content-sharing platform. Just snap a photo, choose a filter to transform the look and feel, add comments (if you like) and share! Our Privacy Policy explains how we and some of the companies we work with collect, use, share and protect information in relation to our mobile services, web site, and any software provided on or in connection with Instagram services (collectively, the \"Service\"), and your choices about the collection and use of your information. By using our Service you understand and agree that we are providing a platform for you to post content, including photos, comments and other materials (\"User Content\"), to the Service and to share User Content publicly. This means that other Users may search for, see, use, or share any of your User Content that you make publicly available through the Service, consistent with the terms and conditions of this Privacy Policy and our Terms of Use (which can be found at http://instagram.com/about/legal/terms/). Our Policy applies to all visitors, users, and others who access the Service (\"Users\").
     Expected result: 
-    {
-        "LLM_output_long": "We collect and share information to improve services. Users can manage preferences. We retain data until account deletion and protect information with encryption. Notifications of policy changes will be sent. We do not respond to Do Not Track.",
-        "general_cat_5": 2,
-        "specific_cat_10": [
-            {
-                "code" : 1,
-                "LLM_output" : "Collection for service improvement.",
-                "LLM_rank": 1
-            },
-            {
-                "code" : 2,
-                "LLM_output" : "Sharing with selected partners.",
-                "LLM_rank": 2
-            },
-            {
-                "code" : 3,
-                "LLM_output" : "User-manageable preferences.",
-                "LLM_rank": 1
-            },
-            {
-                "code" : 4,
-                "LLM_output" : "Data accessible and modifiable.",
-                "LLM_rank": 1
-            },
-            {
-                "code" : 5,
-                "LLM_output" : "Retention until account deletion.",
-                "LLM_rank": 2
-            },
-            {
-                "code" : 6,
-                "LLM_output" : "Protection through encryption.",
-                "LLM_rank": 1
-            },
-            {
-                "code" : 7,
-                "LLM_output" : "Notifications for policy changes.",
-                "LLM_rank": 1
-            },
-            {
-                "code" : 8,
-                "LLM_output" : "We do not respond to Do Not Track.",
-                "LLM_rank": 3
-            },
-            {
-                "code" : 9,
-                "LLM_output" : "Compliance for children's data collection.",
-                "LLM_rank": 1
-            },
-            {
-                "code" : 10,
-                "LLM_output" : "General data collection practices.",
-                "LLM_rank": 2
-            }
-        ]
-    }
+   {
+    "LLM_output_long": "Instagram collects, uses, and shares user content as part of its service, with publicly posted content visible to others. The policy applies to all users and explains how information is managed. The details on data access, user control options, and specific protections are limited, but general information about data collection is provided.",
+    "general_cat_5": 4,
+    "specific_cat_10": [
+        {
+            "code": 1,
+            "LMM_output": "Instagram collects user content and info for service provision.",
+            "LMM_rank": 2
+        },
+        {
+            "code": 2,
+            "LMM_output": "Third parties may access shared user content.",
+            "LMM_rank": 3
+        },
+        {
+            "code": 3,
+            "LMM_output": "User content may be controlled by privacy settings.",
+            "LMM_rank": 2
+        },
+        {
+            "code": 4,
+            "LMM_output": "No details provided on editing or deleting data.",
+            "LMM_rank": 3
+        },
+        {
+            "code": 5,
+            "LMM_output": "No mention of data retention duration.",
+            "LMM_rank": 3
+        },
+        {
+            "code": 6,
+            "LMM_output": "Limited protection details provided.",
+            "LMM_rank": 2
+        },
+        {
+            "code": 7,
+            "LMM_output": "No mention of notification about policy changes.",
+            "LMM_rank": 3
+        },
+        {
+            "code": 8,
+            "LMM_output": "No mention of Do Not Track signals.",
+            "LMM_rank": 3
+        },
+        {
+            "code": 9,
+            "LMM_output": "No specific practices for different user groups.",
+            "LMM_rank": 3
+        },
+        {
+            "code": 10,
+            "LMM_output": "General description of Instagram's service and policy coverage.",
+            "LMM_rank": 0
+        }
+    ]
+}
 
 
 `
